daa9f1072a51d81dca0dfc112038b338409f1ac9
Ahmed K. A <ahmadkabdullah@pm.me>
2022-10-04 19:44:49 +0300

    init: add to git

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="min-w-screen min-h-screen bg-gray-200">
+    <header
+      class="flex flex-col items-center justify-center gap-8 p-8 min-h-screen w-1/2 m-auto"
+    >
+      <h1 class="p-4 text-4xl font-bold text-gray-800">My name's Ahmed</h1>
+      <h1
+        class="bg-gray-500 bg-opacity-20 rounded-2xl p-4 text-2xl font-bold text-gray-800"
+      >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo sit
+        perspiciatis reprehenderit nam perferendis ut obcaecati inventore, quia
+        sed velit rerum possimus, expedita molestiae doloribus dolor delectus
+        harum quasi fugit.
+      </h1>
+    </header>
+    <main
+      class="flex flex-col justify-center items-center min-h-screen w-1/2 m-auto"
+    >
+      <p
+        class="bg-gray-500 bg-opacity-20 rounded-2xl p-4 text-2xl font-bold text-gray-800"
+      >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus at
+        aperiam modi, itaque reiciendis asperiores nemo ipsa consequatur sed
+        doloribus, maiores veritatis laudantium assumenda, eaque ut recusandae.
+        Asperiores consequatur beatae voluptate quia ipsam error nulla ducimus
+        vel et perspiciatis rerum praesentium, quod tempore, ratione iusto sit
+        esse cupiditate doloremque laborum.
+      </p>
+    </main>
+  </div>
+</template>
