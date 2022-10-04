1a7211aecd41e064ada84974ad888ce6a9d8f842
Ahmed K. A <ahmadkabdullah@pm.me>
2022-10-04 22:51:47 +0300

    fix: remove import of vueuse motion package

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-
-import { MotionPlugin } from "@vueuse/motion";
 import "virtual:windi.css";
 
 const app = createApp(App);
-
-app.use(MotionPlugin);
 
 app.mount("#app");
