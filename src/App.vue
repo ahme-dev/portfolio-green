629632c41d9a4c4a85087a57f751e506b5ab830b
Ahmed K. A <ahmadkabdullah@pm.me>
2022-10-04 20:00:44 +0300

    feat: apply more style and structure

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="min-w-screen min-h-screen bg-gray-200">
+  <div id="container" class="min-w-screen min-h-screen bg-warm-gray-200">
+    <!-- Header -->
     <header
-      class="flex flex-col items-center justify-center gap-8 p-8 min-h-screen w-1/2 m-auto"
+      class="flex flex-col items-center justify-center gap-8 p-8 min-h-screen md:( w-1/2 m-auto)"
     >
-      <h1 class="p-4 text-4xl font-bold text-gray-800">My name's Ahmed</h1>
+      <h1 class="p-4 text-4xl font-bold text-warm-gray-800">My name's Ahmed</h1>
       <h1
-        class="bg-gray-500 bg-opacity-20 rounded-2xl p-4 text-2xl font-bold text-gray-800"
+        class="bg-warm-gray-500 text-warm-gray-800 bg-opacity-20 rounded-2xl p-4 text-2xl hover:(shadow-2xl)"
       >
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo sit
         perspiciatis reprehenderit nam perferendis ut obcaecati inventore, quia
@@ -15,11 +16,25 @@
         harum quasi fugit.
       </h1>
     </header>
+    <!-- Header End -->
+
+    <!-- Main -->
     <main
-      class="flex flex-col justify-center items-center min-h-screen w-1/2 m-auto"
+      class="flex flex-col justify-center items-center p-8 gap-8 min-h-screen md:(w-1/2 m-auto)"
     >
+      <h1 class="p-4 text-2xl font-bold self-start">Projects</h1>
       <p
-        class="bg-gray-500 bg-opacity-20 rounded-2xl p-4 text-2xl font-bold text-gray-800"
+        class="bg-warm-gray-500 text-warm-gray-800 bg-opacity-20 rounded-2xl p-4 text-xl"
+      >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus at
+        aperiam modi, itaque reiciendis asperiores nemo ipsa consequatur sed
+        doloribus, maiores veritatis laudantium assumenda, eaque ut recusandae.
+        Asperiores consequatur beatae voluptate quia ipsam error nulla ducimus
+        vel et perspiciatis rerum praesentium, quod tempore, ratione iusto sit
+        esse cupiditate doloremque laborum.
+      </p>
+      <p
+        class="bg-warm-gray-500 text-warm-gray-800 bg-opacity-20 rounded-2xl p-4 text-xl"
       >
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus at
         aperiam modi, itaque reiciendis asperiores nemo ipsa consequatur sed
@@ -29,5 +44,8 @@
         esse cupiditate doloremque laborum.
       </p>
     </main>
+    <!-- Main End -->
   </div>
 </template>
+
+<style></style>
