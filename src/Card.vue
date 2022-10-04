a29e6dbe6828f3fd6c785efd3efdf040d84c9c76
Ahmed K. A <ahmadkabdullah@pm.me>
2022-10-04 20:34:14 +0300

    feat: add component for cards and change styling

---
+++ src/Card.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <p
+    class="bg-gray-500 text-gray-800 bg-opacity-20 rounded-2xl p-4 text-2xl hover:(shadow-md shadow-emerald-300 cursor-pointer)"
+  >
+    <slot></slot>
+  </p>
+</template>
